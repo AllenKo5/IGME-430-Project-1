274e90a01d686b9a45765b86f82c6e56f312cab7
Allen Ko (RIT Student) <ask9458@rit.edu>
2022-09-28 15:50:58 -0400

    set up canvas

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IGME-430-Project-1
+
+Using tutorial from https://flaviocopes.com/canvas-node-generate-image/ to create canvas images
--- client/client.js
+++ client/client.js
@@ -1,5 +1,14 @@
-const init = () => {
+const { createCanvas } = require('canvas');
 
+const init = () => {
+    const width = 1200;
+    const height = 600;
+
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext('2d');
+
+    context.fillStyle('#999');
+    context.fillRect(0, 0, width, height);
 };
 
 window.onload = init;
